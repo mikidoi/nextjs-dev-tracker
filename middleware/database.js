8cb9818f06cf0f3902a3c81d55d5a98053c3dd4a
Miki Doi <ddmikiddpatty@gmail.com>
2020-08-06 14:49:30 +0200

    Create next.config.js and import express

--- middleware/database.js
+++ middleware/database.js
@@ -1,2 +1,22 @@
-//Set up our connection to MongoDB
-//Instantiate the middleware to use it in our API routes
+// Set up our connection to MongoDB
+// Instantiate the middleware to use it in our API routes
+import { MongoClient } from "mongodb";
+import nextConnect from "next-connect";
+
+const client = new MongoClient(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function database(req, res, next) {
+  if (!client.isConnected()) await client.connect();
+  req.dbClient = client;
+  req.db = client.db("kids-dev-tracker");
+  return next();
+}
+
+const middleware = nextConnect();
+
+middleware.use(database);
+
+export default middleware;
--- pages/api/kids.js
+++ pages/api/kids.js
@@ -1,5 +1,14 @@
-export default (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ message: "Hello from the Daily route" }));
-};
+import nextConnect from "next-connect";
+import middleware from "../../middleware/database";
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  let doc = await req.db.collection("kids").findOne();
+  console.log(doc);
+  res.json(doc);
+});
+
+export default handler;
